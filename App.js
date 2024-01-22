ddc30b0d6f600d4b63be882733b5ff9a95bb5c3b
seorinnn <srgongju@naver.com>
2024-01-22 16:35:10 +0900

    레이아웃 구성하기 과제 커밋입니다.

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.box1}></View>
+      <View style={styles.box2}></View>
+      <View style={styles.container2}>
+        <View style={styles.box3}></View>
+        <View style={styles.box4}></View>
+      </View>
+      <View style={styles.box5}></View>
+      <View style={styles.box6}></View>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +23,37 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  container2: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  box1: {
+    width: "100%",
+    flex: 1,
+    backgroundColor: "#00CCFF",
+  },
+  box2: {
+    width: "100%",
+    flex: 1,
+    backgroundColor: "#0033FF",
+  },
+  box3: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  box4: {
+    flex: 1,
+    backgroundColor: "black",
+  },
+  box5: {
+    width: "100%",
+    flex: 1,
+    backgroundColor: "#666666",
+  },
+  box6: {
+    width: "100%",
+    flex: 1,
+    backgroundColor: "#FFFF00",
   },
 });
